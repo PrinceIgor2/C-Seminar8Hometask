1f5d1312b51bb362e4c7a5df8f085c1c51108e3d
Igor <kugimn@list.ru>
2022-10-14 05:59:40 +0300

    56++

--- Task56/Program.cs
+++ Task56/Program.cs
@@ -9,26 +9,19 @@
 */
 
 
-int[,] table = new int[4, 4];
-FillArrayRandom(table);
-PrintArray(table);
-Console.WriteLine();
-NumberRowMinSumElements(table);
-
-
 // Функция вывода номера строки (не индекса) с наименьшей суммой элементов 
-void NumberRowMinSumElements(int[,] array)
+void FindRowMinimalSumOfElements(int[,] array)
 {
     int minRow = 0;
     int minSumRow = 0;
     int sumRow = 0;
-    for (int i = 0; i < table.GetLength(1); i++)
+    for (int i = 0; i < array.GetLength(1); i++)
     {
-        minRow += table[0, i];
+        minRow += array[0, i];
     }
-    for (int i = 0; i < table.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < table.GetLength(1); j++) sumRow += table[i, j];
+        for (int j = 0; j < array.GetLength(1); j++) sumRow += array[i, j];
         if (sumRow < minRow)
         {
             minRow = sumRow;
@@ -52,14 +45,20 @@ void PrintArray(int[,] array)
     }
 }
 
-// Функция заполнения массива рандомно числами от 1 до 9
-void FillArrayRandom(int[,] array)
+// Функция заполнения массива рандомно числами от 1 до 19
+void FillRandomArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(1, 10);
+            array[i, j] = new Random().Next(1, 20);
         }
     }
 }
+
+int[,] array = new int[4, 4];
+FillRandomArray(array);
+PrintArray(array);
+Console.WriteLine();
+FindRowMinimalSumOfElements(array);
